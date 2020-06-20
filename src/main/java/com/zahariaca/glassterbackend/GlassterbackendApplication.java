2b610bebadc449d414144c8557a780b58dce486c
zahariaca <zaharia.c.alexandru@gmail.com>
2020-06-20 12:12:19 +0300

    Adds spring initializr starter project

---
+++ src/main/java/com/zahariaca/glassterbackend/GlassterbackendApplication.java
@@ -0,0 +1,13 @@
+package com.zahariaca.glassterbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassterbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlassterbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zahariaca/glassterbackend/GlassterbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zahariaca.glassterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassterbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
